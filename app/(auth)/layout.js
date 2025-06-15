c3ceca48d40596a6f4819c4faf4f80b7990c5411
yusiuuu <mohammedyusufy70@gmail.com>
2025-06-15 12:07:52 +0530

    Initial commit: Setup Next.js with ShadCN, layout, auth, header

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayout = ({children}) => {
+  return (
+    <div className="flex justify-center pt-20">{children}</div>
+  )
+};
+
+export default AuthLayout;
---
+++ components/header.jsx
@@ -0,0 +1,51 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import { Button } from './ui/button';
+import { LayoutDashboard, PenBox } from 'lucide-react';
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+        <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+            <Link href={"/"}>
+                <Image src={"/logo.png"} alt="Wealth Logo" width={200} height={60} className="h-12 w-auto object-contain" />
+            </Link>
+
+        <div className='flex items-center space-x-6'>
+            <SignedIn>
+                <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+                    <Button variant="outline">
+                        <LayoutDashboard size={15}/>
+                        <span className="hidden md:inline">Dashboard</span>
+                    </Button>
+                </Link>
+
+                <Link href={"/transaction/create"}>
+                    <Button className="flex items-center gap-2">
+                        <PenBox size={15}/>
+                        <span className="hidden md:inline">Transaction</span>
+                    </Button>
+                </Link>
+            </SignedIn>
+
+            <SignedOut>
+                <SignInButton forceRedirectUrl='/dashboard'>
+                    <Button variant="outline">Login</Button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton appearance={{
+                elements:{
+                    avatarBox: "h-10 w-10",
+                }
+              }}/>
+            </SignedIn>
+        </div>
+        </nav>
+    </div>
+  );
+};
+
+export default Header
