927e62c9769bfc9ea64341abd641abd349ea76dc
yusiuuu <mohammedyusufy70@gmail.com>
2025-06-21 20:51:23 +0530

    feat: render user accounts, change default account, account page, seed transactions data

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,27 +1,71 @@
+"use client";
+
+
+import { updateDefaultAccount } from '@/actions/accounts';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Switch } from '@/components/ui/switch';
+import useFetch from '@/hooks/use-fetch';
 import { ArrowDownRight, ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect } from 'react'
+import { toast } from 'sonner';
 
 const AccountCard = ({ account }) => {
     const { name, type, balance, id, isDefault } = account;
+
+    const {
+        loading: updateDefaultLoading,
+        fn: updateDefaultFn,
+        data: updatedAccount,
+        error,
+    } = useFetch(updateDefaultAccount);
+
+    const handleDefaultChange =async (event)=> {
+        event.preventDefault();
+
+        if(isDefault){
+            toast.warning("You need atleast 1 default account");
+            return;
+            //DOn't allow toggling off the default account
+        }
+
+        await updateDefaultFn(id);
+    }
+
+    useEffect(()=>{
+        if(updatedAccount?.success) {
+            toast.success("Default account updated successfully");
+        }
+    }, [updatedAccount, updateDefaultLoading]);
+
+    useEffect(()=>{
+        if(error) {
+            toast.error(error.message || "Failed to update Default account");
+        }
+    }, [error]);
+
   return (
-    <Card>
+    <Card className="hover:shadow-md transition-shadow group relative">
         <Link href={`/account/${id}`}>
-            <CardHeader>
-                <CardTitle>{name}</CardTitle>
-                <Switch/>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium capitalize">
+                    {name}
+                </CardTitle>
+                <Switch 
+                checked={isDefault}
+                onClick={handleDefaultChange}
+                disabled={updateDefaultLoading}
+                />
             </CardHeader>
             <CardContent>
                 <div className='text-2xl font-bold'>
-                    ₨ {parseFloat(balance).toFixed(2)}
+                    ${parseFloat(balance).toFixed(2)}
                 </div>
                 <p className='text-xs text-muted-foreground'>
                     {type.charAt(0)+ type.slice(1).toLowerCase()} Account
                 </p>
             </CardContent>
-            <CardFooter>
+            <CardFooter className="flex justify-between text-sm text-muted-foreground">
                 <div className="flex items-center">
                     <ArrowUpRight className='mr-1 h-4 w-4 text-green-500'/>
                     Income
