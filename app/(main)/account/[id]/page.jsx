7f56fda2f02759a80fadcdd22df2b97ce3d9eaa8
yusiuuu <mohammedyusufy70@gmail.com>
2025-07-29 12:50:32 +0530

    Added BarChart to show transactions

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation';
 import React, { Suspense } from 'react'
 import TransactionTable from '../_components/transaction-table';
 import { BarLoader } from 'react-spinners';
+import {AccountChart} from '../_components/account-chart';
 
 const AccountsPage = async({params}) => {
     const accountData = await getAccountWithTransactions(params.id);
@@ -28,6 +29,9 @@ const AccountsPage = async({params}) => {
         </div>
 
         {/*Chart Section*/}
+        <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='#9333ea'/>}>
+        <AccountChart transactions={transactions}/>
+        </Suspense>
 
         {/*Transaction Table*/}
 
